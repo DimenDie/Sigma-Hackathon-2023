0ece0f15aa286dde7dee18db2f3acc2f315054d2
DimenDie <dimarepalo3@gmail.com>
2023-01-31 17:06:22 +0200

    swinging implemented

--- Assets/Scripts/Swinging.cs
+++ Assets/Scripts/Swinging.cs
@@ -14,16 +14,26 @@ public class Swinging : MonoBehaviour
     private Vector3 swingPoint;
     private SpringJoint joint;
 
+    private Vector3 currentGrapplePosition;
+
     private void Update()
     {
+        shootCenter.eulerAngles = Vector3.zero;
         if (Input.GetKeyDown(swingKey)) StartSwing();
         if (Input.GetKeyUp(swingKey)) StopSwing();
     }
+    private void LateUpdate()
+    {
+        DrawRope();
+    }
     void StartSwing()
     {
+        print("start swing called");
         RaycastHit hit;
-        if(Physics.Raycast(camera.position, camera.forward, out hit, maxSwingDistance, isGrappleable))
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, maxSwingDistance, isGrappleable))
         {
+            print("raycasted");
             swingPoint = hit.point;
             joint = player.gameObject.AddComponent<SpringJoint>();
             joint.autoConfigureConnectedAnchor = false;
@@ -38,11 +48,23 @@ public class Swinging : MonoBehaviour
             joint.damper = 7f;
             joint.massScale = 4.5f;
 
+            lineRenderer.positionCount = 2;
+            currentGrapplePosition = shootCenter.position;
+
         }
     }
     void StopSwing()
     {
+        print("stop swing called");
         lineRenderer.positionCount = 0;
         Destroy(joint);
+    }
+
+    void DrawRope()
+    {
+        if (!joint) return;
+
+        lineRenderer.SetPosition(0, shootCenter.position);
+        lineRenderer.SetPosition(1, swingPoint);
     }
 }
