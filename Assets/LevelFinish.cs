0bf1e0fd807a8ac68d6452c52ffbf94c15009695
DimenDie <dimarepalo3@gmail.com>
2023-02-01 17:36:19 +0200

    save system

--- Assets/LevelFinish.cs
+++ Assets/LevelFinish.cs
@@ -1,20 +1,32 @@
+using Newtonsoft.Json;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class LevelFinish : MonoBehaviour
 {
     public GameObject UIManager;
     public GameObject ResultPanel;
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
         {
             Debug.Log("Finish!!");
+
+
+            UIManager.GetComponent<Level>().levelFinished = true;
+
+            SaveManager.Save("GhostReplays", "test", UIManager.GetComponent<Level>().saveCoords);
+
             UIManager.GetComponent<UI>().TogglePause();
             ResultPanel.SetActive(true);
             ResultPanel.GetComponentInChildren<TextMeshProUGUI>().text = UIManager.GetComponent<Level>().CheckMedal();
+
+
+
         }
     }
 }
--- Assets/Scripts/LevelsScript/Level.cs
+++ Assets/Scripts/LevelsScript/Level.cs
@@ -5,19 +5,42 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class Level : MonoBehaviour
 {
     public int bunny;
     public int cheetah;
     public int lightning;
 
-    public GameObject player;
+    public GameObject playerPrefab;
+    public GameObject ghostPrefab;
     public GameObject levelStart;
     public GameObject levelEnd;
+    public GameObject playerObj;
+    public GameObject ghostObj;
+
+    public bool levelFinished;
+
+    private string saveName;
+
+    [HideInInspector] public SaveCoords saveCoords; //coords to write into a save file of ghost replay
+    public SaveCoords loadCoords;
+
+    
 
     private void Start()
     {
-        Instantiate(player, levelStart.transform.position, levelStart.transform.rotation);
+        playerObj = Instantiate(playerPrefab, levelStart.transform.position, levelStart.transform.rotation);
+        ghostObj = Instantiate(ghostPrefab, levelStart.transform.position, levelStart.transform.rotation);
+
+        if (index < SaveManager.Load<SaveCoords>("GhostReplays", "test").savePositions.Count)
+        {
+            loadCoords.savePositions = SaveManager.Load<SaveCoords>("GhostReplays", "test").savePositions;
+            loadCoords.saveRotations = SaveManager.Load<SaveCoords>("GhostReplays", "test").saveRotations;
+            index++;
+        }
+
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
 
@@ -28,6 +51,22 @@ public class Level : MonoBehaviour
             Restart();
         }
 
+    }
+    int index;
+    private void FixedUpdate()
+    {
+        if (index < loadCoords.savePositions.Count)
+        {
+            ghostObj.transform.position = loadCoords.savePositions[index];
+            ghostObj.transform.rotation = loadCoords.saveRotations[index];
+            index++;
+        }
+        //Debug.Log(playerObj.transform.GetChild(0).position);
+        if (!levelFinished)
+        {
+            saveCoords.savePositions.Add(playerObj.transform.GetChild(0).position);
+            saveCoords.saveRotations.Add(playerObj.transform.GetChild(0).rotation);
+        }
     }
 
     public string CheckMedal()
@@ -54,4 +93,11 @@ public class Level : MonoBehaviour
     {
         SceneManager.LoadScene(0);
     }
+}
+
+[Serializable]
+public struct SaveCoords
+{
+    public List<Vector3> savePositions;
+    public List<Quaternion> saveRotations;
 }
