2725f15d4b0489872fd3914f1161718b5bd2579b
LavandoviTrash <senia.azatkhanov@gmail.com>
2023-02-03 22:57:35 +0200

    SpawnDeathShaders

--- Assets/Scripts/Player/ShaderBehaviour.cs
+++ Assets/Scripts/Player/ShaderBehaviour.cs
@@ -4,15 +4,69 @@ using UnityEngine;
 
 public class ShaderBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Material sphereMaterial, rootsMaterial;
+    [SerializeField] float dissolveSpeed;
+    [SerializeField] float defaultThiccness;
+    [SerializeField] AnimationCurve curve;
+    bool duringDissolve, onoff;
+    
+
+    private void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.O) && !duringDissolve)
+        {
+            StartCoroutine(DissolveSwitch(onoff));
+            onoff = !onoff;
+        }
     }
+
+    IEnumerator DissolveSwitch( bool on )
+    {
+        float t = 0;
+        float startValue = on ? 0 : 1f;
+        float targetValue = on ? 1f : 0;
+        float value;
+
+        sphereMaterial.SetFloat("_Thiccness", defaultThiccness);
+        rootsMaterial.SetFloat("_Thiccness", defaultThiccness);
+
+        sphereMaterial.SetInt("_Dead", System.Convert.ToInt32(targetValue));
+        rootsMaterial.SetInt("_Dead", System.Convert.ToInt32(targetValue));
+
+        duringDissolve = true;
+
+        while(t<=1)
+        {
+            value = Mathf.Lerp(startValue, targetValue, curve.Evaluate(t));
+            sphereMaterial.SetFloat("_DissolveStatus", value);
+            rootsMaterial.SetFloat("_DissolveStatus", value);
+            t += Time.deltaTime / dissolveSpeed;
+            yield return null;
+        }
+
+        sphereMaterial.SetFloat("_DissolveStatus", targetValue);
+        rootsMaterial.SetFloat("_DissolveStatus", targetValue);
+
+        t = 0;
+
+        while(t<=1)
+        {
+            value = Mathf.Lerp(defaultThiccness, 0, curve.Evaluate(t));
+            sphereMaterial.SetFloat("_Thiccness", value);
+            rootsMaterial.SetFloat("_Thiccness", value);
+            t += Time.deltaTime / (dissolveSpeed / 2);
+            yield return null;
+        }
+
+        sphereMaterial.SetFloat("_Thiccness", 0);
+        rootsMaterial.SetFloat("_Thiccness", 0);
+
+        duringDissolve = false;
+    }
+
 }
